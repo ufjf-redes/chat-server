f70665637a23c2c7c64c6ba0be665f342ff7aa81
williancsmelo <melo.williancs@gmail.com>
2024-05-22 20:44:58 -0300

    criando endpoint para registro do cliente e listar clientes

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request, Response
+
+clientsDict = dict()
+
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
@@ -6,7 +9,23 @@ def hello_world():
 
 @app.post("/registrar-cliente")
 def registrarCliente():
-    pass
+    clientName: str = request.form.get('name')
+    clientAddress: str = request.form.get('address')
+    if (not clientName or not clientName.isidentifier()): 
+        return "Nome de cliente inválido", 404
+    if(not clientAddress):
+        return "Endereço não encontrado no corpo da requisição", 404
+    
+    resp = Response(status=200)
+    if(clientsDict.get(clientName)):
+        resp.data = "O cliente já estava registrado e foi sobrescrito"
+    clientsDict[clientName] = clientAddress
+    
+    return resp
+
+@app.get("/lista-clientes")
+def listaClientes():
+    return list(clientsDict.keys())
 
 if __name__ == "__main__":
     app.run(debug=True)
